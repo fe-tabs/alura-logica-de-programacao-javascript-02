51fabec9334ebb720f2eb4a20fa79130a628ff60
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-25 20:52:46 -0300

    :sparkles: feat: tell if guess is less or greater than secret number

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+let secretNumber = generateRandomNumber();
+
 let title = document.querySelector('h1');
 title.innerHTML = "Jogo do Número Secreto";
 
 let paragraph = document.querySelector('p');
-paragraph.innerHTML = "Escolha um número entre 1 e 100: ";
+paragraph.innerHTML = "Escolha um número entre 1 e 10: ";
+
+function generateRandomNumber() {
+  return parseInt(Math.random() * 10) + 1;
+}
+
+function checkGuess() {
+  let guess = document.querySelector('input').value;
+
+  if (guess == secretNumber) {
+    paragraph.innerHTML = `Você acertou o número secreto: ${secretNumber}!`;
+  } else {
+    if (guess > secretNumber) {
+      paragraph.innerHTML = `Você errou! O número secreto é menor que ${guess}!`;
+    } else {
+      paragraph.innerHTML = `Você errou! O número secreto é maior que ${guess}!`;
+    }
+  }
+}
