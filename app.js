de1aab51e6bcc3ab5661ecc088aba0bd7e2480d9
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-25 20:18:20 -0300

    :sparkles: feat: set heading with javascript

---
+++ app.js
@@ -0,0 +1,5 @@
+let title = document.querySelector('h1');
+title.innerHTML = "Jogo do Número Secreto";
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = "Escolha um número entre 1 e 100: ";
